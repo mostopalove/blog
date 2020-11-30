3aa213df04c9b2ec4c3eb6fb5a9166dbe5a69409
mostopalove <Most09342262007>
2020-11-30 15:39:00 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface'
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCsxQXLifMpD3nErrrcQo_Ga2EhmzKH5w8',
+  fbDbUrl: 'https://angular-blog-am.firebaseio.com'
+};
